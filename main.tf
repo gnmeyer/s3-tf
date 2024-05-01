8d65b19ba6be190897173158f5dda6d417eec46b
gnmeyer <grant.meyer@liatrio.com>
2024-05-01 12:30:56 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  backend "s3" {
+    bucket = "awsbucket-grant"
+    key    = "state "
+    region = "us-east-1"
+  }
+
+  required_version = ">= 1.2.0"
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0fe630eb857a6ec83"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = ["sg-0664e17c27dc38c48"]
+
+  subnet_id = "subnet-019c03f7b62349192"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
